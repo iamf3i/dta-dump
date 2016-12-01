5e87cae1e70b148c29e4dd7137405bc027417a2f
Fawzi NASSIM <fawzi.nassim@gmail.com>
2016-12-01 16:04:53 +0100

    Observer Design pattern base

---
+++ dta-chat/src/dta/chat/model/ChatConversationModel.java
@@ -0,0 +1,14 @@
+package dta.chat.model;
+
+import dta.chat.model.observer.ChatObservable;
+
+public class ChatConversationModel<T> extends ChatObservable<T> {
+
+	public void setLogin(String login) {
+
+	}
+
+	public void sendMessage(String msg) {
+
+	}
+}
---
+++ dta-chat/src/dta/chat/model/observer/ChatObservable.java
@@ -0,0 +1,20 @@
+package dta.chat.model.observer;
+
+import java.util.List;
+
+public abstract class ChatObservable<T> {
+
+	List<ChatObserver<T>> observers;
+
+	public void addObserver(ChatObserver<T> observer) {
+
+	}
+
+	public void removeObserver(ChatObserver<T> observer) {
+
+	}
+
+	public void notifyObservers(T msg) {
+
+	}
+}

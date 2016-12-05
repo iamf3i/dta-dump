3a490eccc6717d1dd89f44bafbfd90efc6dd7115
Fawzi NASSIM <fawzi.nassim@gmail.com>
2016-12-05 15:58:08 +0100

    Implements config file with dao parameter

--- pizzeria-parent/pizzeria-console/src/main/java/fr/dta/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ pizzeria-parent/pizzeria-console/src/main/java/fr/dta/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,16 +1,22 @@
 package fr.dta.pizzeria.console;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 import fr.dta.pizzeria.console.ihm.IhmUtil;
 import fr.dta.pizzeria.console.ihm.MainMenu;
-import fr.dta.pizzeria.dao.ArrayDaoFactory;
+import fr.dta.pizzeria.dao.DaoFactory;
 import fr.dta.pizzeria.model.Pizza;
 
 public class PizzeriaAdminConsoleApp {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 
-		IhmUtil utils = new IhmUtil(new Scanner(System.in), new ArrayDaoFactory());
+		ResourceBundle bundle = ResourceBundle.getBundle("application");
+		String daoConfig = bundle.getString("dao.impl");
+		DaoFactory daoFactory = (DaoFactory) Class.forName(daoConfig).newInstance();
+
+		IhmUtil utils = new IhmUtil(new Scanner(System.in), daoFactory);
 		MainMenu core = new MainMenu(utils);
 
 		core.run();

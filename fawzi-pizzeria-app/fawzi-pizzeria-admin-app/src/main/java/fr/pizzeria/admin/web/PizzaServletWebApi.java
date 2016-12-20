be8bae2d512a56a2e95257f3949ca087406c4d84
Fawzi NASSIM <fawzi.nassim@gmail.com>
2016-12-20 12:28:09 +0100

    Create sub module for Pizzeria Admin Web App

---
+++ fawzi-pizzeria-app/fawzi-pizzeria-admin-app/src/main/java/fr/pizzeria/admin/web/PizzaServletWebApi.java
@@ -0,0 +1,45 @@
+package fr.pizzeria.admin.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PizzaServletWebApi
+ */
+public class PizzaServletWebApi extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public PizzaServletWebApi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: LOLOLOL").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

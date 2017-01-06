095c48e8012bb1c69de3b3b7c4a08628e6a66039
Fawzi NASSIM <fawzi.nassim@gmail.com>
2017-01-06 17:20:46 +0100

    Implements Spring data jpa DAO for pizzeria-console

--- fawzi-pizzeria-app/fawzi-pizzeria-console/src/main/java/fr/dta/pizzeria/console/PizzeriaAppSpringConfig.java
+++ fawzi-pizzeria-app/fawzi-pizzeria-console/src/main/java/fr/dta/pizzeria/console/PizzeriaAppSpringConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -17,11 +22,26 @@ import java.util.Scanner;
  */
 @Configuration
 @ComponentScan({"fr.dta.pizzeria.console", "fr.dta.pizzeria.dao"})
+@EnableTransactionManagement
+@EnableJpaRepositories("fr.dta.pizzeria.dao")
 public class PizzeriaAppSpringConfig {
 
     @Bean
-    public DataSource getDataSource(){
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
 
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+
+        LocalEntityManagerFactoryBean emf = new LocalEntityManagerFactoryBean();
+
+        emf.setPersistenceUnitName("JPAUNIT");
+        return emf;
+    }
+
+    @Bean
+    public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/pizzeria?useSSL=false");
@@ -31,7 +51,7 @@ public class PizzeriaAppSpringConfig {
     }
 
     @Bean
-    public Scanner getScanner(){
+    public Scanner getScanner() {
         return new Scanner(System.in);
     }
 

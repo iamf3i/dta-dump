c5e909421db299d092e0ac4e747e117e0fa21c34
Fawzi NASSIM <fawzi.nassim@gmail.com>
2017-01-18 14:32:55 +0100

    Manage User First version

---
+++ JavaScript/dtang/src/app.js
@@ -0,0 +1,37 @@
+import angular from 'angular';
+import ngRoute from 'angular-route';
+
+import UserModule from './user';
+
+import { ExclamationFilter } from './exclamation.filter';
+
+angular.module('app', [
+  ngRoute,
+  UserModule
+])
+.filter('exclamation', ExclamationFilter)
+.value('Version', '1.0.0')
+
+.config(function($routeProvider, $locationProvider) {
+
+  $locationProvider.html5Mode(true);
+
+  $routeProvider
+    .when('/', {
+      template: '<h1>Bienvenue {{ ctrl.name }}</h1>',
+      controller: function () {
+        this.name = 'Thomas';
+      },
+      controllerAs: 'ctrl'
+    })
+    .when('/about', {
+      template: '<h1>About</h1>'
+    })
+
+    .otherwise('/');
+
+})
+
+.run(function() {
+  console.log('PizzaYOLO !');
+})
---
+++ JavaScript/dtang/src/user/user.service.js
@@ -0,0 +1,38 @@
+const api = 'http://localhost:3000/users';
+
+export class UserService {
+    constructor($http, $timeout) {
+        this.$http = $http;
+        this.$timeout = $timeout;
+    }
+
+    getUser(id) {
+        return this.$http.get(`${ api }/${ id }`)
+            .then(response =>  response.data);
+    }
+
+    getUsers() {
+        return this.$http.get(api)
+            .then(response =>  response.data);
+    }
+
+    saveUser(user) {
+        return this[user.id ? 'updateUser' : 'addUser'](user);
+    }
+
+    addUser(user) {
+        return this.$http.post(api, user)
+            .then(response =>  response.data);
+    }
+
+    updateUser(user) {
+        return this.$http.put(`${ api }/${ user.id }`, user)
+            .then(response =>  response.data);  
+    }
+
+    deleteUser(user) {
+        return this.$timeout(3000)
+            .then(() => this.$http.delete(`${ api }/${ user.id }`))
+            .then(response =>  response.data);
+    }
+}
